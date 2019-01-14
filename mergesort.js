4487b06c7b4eaed93f22517c92b45736743e2387
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-01-14 14:43:57 -0600

    completed merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,12 +1,38 @@
-function split(wholeArray) {
-  
+function split(arr) {
+  const middle = Math.floor(arr.length / 2);
+  const firstHalf = arr.slice(0, middle);
+  const secondHalf = arr.slice(middle);
   return [firstHalf, secondHalf];
 }
 
 function merge(arr1, arr2) {
-  return mergedArr;
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j])
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i])
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j])
+    j++;
+  }
+  return results;
 }
 
 function mergeSort(arr) {
-
+  if (arr.length <= 1) return arr;
+  const splitArr = split(arr);
+  const firstHalf = mergeSort(splitArr[0]);
+  const secondHalf = mergeSort(splitArr[1]);
+  return merge(firstHalf, secondHalf);
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,10 +1,10 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    expect( split([1, 2, 3, 4]) ).toEqual( [1, 2], [3, 4] );
+    expect( split([1, 2, 3, 4]) ).toEqual( [[1, 2], [3, 4]] );
   });
 
   it('is able to handle odd length arrays', function() {
-    expect( split([1, 2, 3, 4, 5]) ).toEqual( [1, 2], [3, 4, 5] );
+    expect( split([1, 2, 3, 4, 5]) ).toEqual( [[1, 2], [3, 4, 5]] );
   });
 });
 

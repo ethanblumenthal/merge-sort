e12d5689d5c5c56e0040752b94cd0b6f46a96a0c
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-01-14 13:38:45 -0600

    added test specs

---
+++ mergesort.js
@@ -0,0 +1,12 @@
+function split(wholeArray) {
+  
+  return [firstHalf, secondHalf];
+}
+
+function merge(arr1, arr2) {
+  return mergedArr;
+}
+
+function mergeSort(arr) {
+
+}
---
+++ mergesort.spec.js
@@ -0,0 +1,21 @@
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function() {
+    expect( split([1, 2, 3, 4]) ).toEqual( [1, 2], [3, 4] );
+  });
+
+  it('is able to handle odd length arrays', function() {
+    expect( split([1, 2, 3, 4, 5]) ).toEqual( [1, 2], [3, 4, 5] );
+  });
+});
+
+describe('Merge function', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect( merge([1, 2], [3, 4, 5]) ).toEqual( [1, 2, 3, 4, 5] );
+  });
+});
+
+describe('Merge Sort function', function(){
+  it('is able to correctly sort an array', function(){
+    expect( mergeSort([3, 5, 1, 2, 4]) ).toEqual( [1, 2, 3, 4, 5] );
+  });
+});
